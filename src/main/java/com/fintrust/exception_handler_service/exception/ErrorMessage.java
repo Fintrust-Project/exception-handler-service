dd7dd3e9541bff5a101dc03e271f25d22f3839e7
SauravME <108649610+SauravME@users.noreply.github.com>
2025-05-20 23:51:22 +0530

    Added json parse exception handling

--- src/main/java/com/fintrust/exception_handler_service/exception/ErrorMessage.java
+++ src/main/java/com/fintrust/exception_handler_service/exception/ErrorMessage.java
@@ -1,11 +1,15 @@
 package com.fintrust.exception_handler_service.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorMessage {
     String errorCode;
     String description;
---
+++ src/main/java/com/fintrust/exception_handler_service/exception/HttpMessageNotReadableExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.fintrust.exception_handler_service.exception;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@Slf4j
+public class HttpMessageNotReadableExceptionHandler implements ExceptionHandler{
+    @Override
+    @PostConstruct
+    public void init() {
+        ExceptionHandlerFactory.add(HttpMessageNotReadableException.class, this);
+    }
+
+    @Override
+    public ResponseEntity<?> handleException(Exception e) {
+        String errorDetails = "unacceptable json";
+        if(e.getCause() instanceof InvalidFormatException){
+            final InvalidFormatException invalidFormatException = (InvalidFormatException) e.getCause();
+            if(invalidFormatException.getTargetType().isEnum()){
+                errorDetails = String.format("Invalid enum values: '%s' . The value must be one of : '%s'", invalidFormatException.getValue(),
+                        invalidFormatException.getPath().get(invalidFormatException.getPath().size()-1).getFieldName(),
+                        Arrays.toString(invalidFormatException.getTargetType().getEnumConstants()));
+            }
+        }
+        log.error("Exception occurred while parsing json {}", e.getMessage());
+        return new ResponseEntity<>(ErrorMessage.builder().description(errorDetails).httpStatus(HttpStatus.BAD_REQUEST).build(), HttpStatus.BAD_REQUEST);
+    }
+}
